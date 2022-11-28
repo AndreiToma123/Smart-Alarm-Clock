d33378d452688199f931dcbcaa703e675db63296
AndreiToma123 <Andrei.Toma@student.lut.fi>
2022-11-28 15:33:35 +0200

    camera code

--- CameraUi.py
+++ CameraUi.py
@@ -1,26 +1,54 @@
 from kivy.app import App
+
 from kivy.uix.camera import Camera
+
 from kivy.uix.boxlayout import BoxLayout
+
 from kivy.uix.button import Button
 
 
-class camera_app(App):
-    def make(self):
-        self.obj_camera = Camera()
-        self.obj_camera.resolution = (810, 810)
-        obj_button = Button(text="Click!")
-        obj_button.size_hint = (.6, .3)
-        obj_button.pos_hint = {'x': .35, 'y': .35}
-        obj_button.bind(on_press=self.selfie())
-        obj_layout = BoxLayout()
-        obj_layout.add_widget(self.obj_camera)
-        obj_layout.add_widget(obj_button)
-        return obj_layout
+class CameraExample(App):
 
-    def selfie(self, *args):
-        print("Selfie has been taken")
-        self.obj_camera.export_to_png('./demoselfie.png')
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
 
+        # Create a camera object
+
+        self.cameraObject = Camera(play=False)
+
+        self.cameraObject.play = True
+
+        self.cameraObject.resolution = (300, 300)  # Specify the resolution
+
+        # Create a button for taking photograph
+
+        self.camaraClick = Button(text="Take Photo")
+
+        self.camaraClick.size_hint = (.5, .2)
+
+        self.camaraClick.pos_hint = {'x': .25, 'y': .75}
+
+        # bind the button's on_press to onCameraClick
+
+        self.camaraClick.bind(on_press=self.onCameraClick)
+
+        # add camera and button to the layout
+
+        layout.add_widget(self.cameraObject)
+
+        layout.add_widget(self.camaraClick)
+
+        # return the root widget
+
+        return layout
+
+    # Take the current frame of the video as the photo graph
+
+    def onCameraClick(self, *args):
+        self.cameraObject.export_to_png('/venv/selfie.png')
+
+
+# Start the Camera App
 
 if __name__ == '__main__':
-    camera_app().run()
+    CameraExample().run()
