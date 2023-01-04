ca894d7ba0dc0e685c24224931051a1804f2d5b6
Trgla <103451393+Trgla@users.noreply.github.com>
2023-01-04 15:22:33 +0200

    Update image_detection.py

--- image_detection.py
+++ image_detection.py
@@ -2,7 +2,7 @@
 Installation:
 go to https://pytorch.org/get-started/locally/ and select the options suitable for installing pytorch in your computer then copy the command and run it.
 type: 
-git clone https://github.com/ultralytics/yolov5  #for cloning
+git clone https://github.com/ultralytics/yolov5  #for cloning yolov5
 cd yolov5
 pip install -r requirements.txt  # installing dependencies
 """
@@ -11,7 +11,10 @@ import torch
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
 img = "selfie.png"   #file "selfie.png" will be automatically generated when you take a selfie from CameraUi.py
-#img = "pic1.jpeg"   #or img = "pic2.jpg" if you want to test without taking a selfie if your camera has low quality
+
+#if you want to test without taking a selfie if your camera has low quality
+#img = "pic1.jpeg"   #or
+#img = "pic2.jpg" 
 
 results = model(img)
 results.print()
