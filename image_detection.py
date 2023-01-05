cd98a7127242d16a0f8a0bd7fb5daff4e8d95d3b
AndreiToma123 <Andrei.Toma@student.lut.fi>
2023-01-05 19:00:02 +0200

    Update image_detection.py

--- image_detection.py
+++ image_detection.py
@@ -11,15 +11,16 @@ pip install -r requirements.txt  # installing dependencies
 
 import torch
 
+
 class ImageDetection:
     def __init__(self, image):
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='final.pt', force_reload=True)
         self.results = self.model(image)
-        self.results.print() #result 
-        #self.results.show() #if you want to see the detection on picture
+        self.results.print()  # result
+        # self.results.show() #if you want to see the detection on picture
+
 
 if __name__ == "__main__":
     ImageDetection("pic1.jpeg")
-    #file "selfie.png" will be automatically generated when you take a selfie from CameraUi.py
-    #if you want to test without taking a selfie, change "selfie.png" to "pic1.jpeg" or "pic2.jpg" or "Face2.png".
-
+    # file "selfie.png" will be automatically generated when you take a selfie from CameraUi.py
+    # if you want to test without taking a selfie, change "selfie.png" to "pic1.jpeg" or "pic2.jpg" or "Face2.png".
