56074f23eafb2f3e7aac0c7a2b7a9dab5731dee0
Trgla <103451393+Trgla@users.noreply.github.com>
2023-01-05 17:29:54 +0200

    Object and face detection

--- image_detection.py
+++ image_detection.py
@@ -1,4 +1,6 @@
 """
+Objects and face detection on images
+
 Installation:
 go to https://pytorch.org/get-started/locally/ and select the options suitable for installing pytorch in your computer then copy the command and run it.
 type: 
@@ -9,15 +11,15 @@ pip install -r requirements.txt  # installing dependencies
 
 import torch
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=True)
-img = "selfie.png"   #file "selfie.png" will be automatically generated when you take a selfie from CameraUi.py
+class ImageDetection:
+    def __init__(self, image):
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='final.pt', force_reload=True)
+        self.results = self.model(image)
+        self.results.print() #result 
+        self.results.show() #if you want to see the detection on picture
 
-#if you want to test without taking a selfie if your camera has low quality
-#img = "pic1.jpeg"   #or
-#img = "pic2.jpg" 
-
-results = model(img)
-results.print()
-#results.show() #if you want to see the detection on picture
-
+if __name__ == "__main__":
+    ImageDetection("selfie.png") 
+    #file "selfie.png" will be automatically generated when you take a selfie from CameraUi.py
+    #if you want to test without taking a selfie, change "selfie.png" to "pic1.jpeg" or "pic2.jpg" or "Face2.png".
 
