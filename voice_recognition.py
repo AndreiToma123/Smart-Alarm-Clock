7b11c35089506ff1ea0c03aa4d44835a8e79cb51
AndreiToma123 <Andrei.Toma@student.lut.fi>
2023-01-05 18:38:10 +0200

    Update voice_recognition.py

--- voice_recognition.py
+++ voice_recognition.py
@@ -26,31 +26,35 @@ def SpeakText(command):
 
 
 r = sr.Recognizer()
-try:
-    with sr.Microphone() as source2:
-        SpeakText(mytext2)  # SpeakText is used to make the program say whatever we want
 
-        while True:
-            print("Say it now")
 
-            # the code to ask the user to say the sentence
-            r.adjust_for_ambient_noise(source2, duration=0.9)
-            audio2 = r.listen(source2)
-            Myvoice = r.recognize_google(audio2)
-            Myvoice = Myvoice.lower()
-            print(Myvoice)
+def Listen():
+    try:
+        with sr.Microphone() as source2:
+            SpeakText(mytext2)  # SpeakText is used to make the program say whatever we want
 
-            # checking whether the sentence matches what the user said
-            if (Myvoice == mytext2):
-                SpeakText("it is correct")
-                break  # the code stops when the sentence matches
-            else:
-                SpeakText(
-                    "it is wrong. try again")  # the code repeats and the user tries again if the sentence does not match
+            while True:
+                print("Say it now")
 
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+                # the code to ask the user to say the sentence
+                r.adjust_for_ambient_noise(source2, duration=0.9)
+                audio2 = r.listen(source2)
+                Myvoice = r.recognize_google(audio2)
+                Myvoice = Myvoice.lower()
+                print(Myvoice)
 
-except sr.UnknownValueError:
-    print("unknown error occurred")
+                # checking whether the sentence matches what the user said
+                if (Myvoice == mytext2):
+                    SpeakText("it is correct")
+                    break  # the code stops when the sentence matches
+                else:
+                    SpeakText(
+                        "it is wrong. try again")  # the code repeats and the user tries again if the sentence does not match
 
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("unknown error occurred")
+
+Listen()
